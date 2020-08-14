d281996873a5c61146c7959a0e0e67131ec6b2e6
Seeulaterrr <natalyakaplun8@gmail.com>
2020-08-15 00:16:53 +0300

    This is my first exercise

--- Lesson_1/Exercise1.py
+++ Lesson_1/Exercise1.py
@@ -1,4 +1,10 @@
-print("HELLO WORD")
+#name=input("Enter name of the best plase in the world ")
+#print(name + ", is the best place \n... in the world")
 
-for i in range(10):
-    print(i)
+#import time
+#print(time.ctime())
+
+pupil="BEN"
+old=16
+grade=9.2
+print("It is %s, %d. Level: %.2f" % (pupil, old, grade))
