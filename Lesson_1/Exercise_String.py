873e5ddb1d8f7d472165fbea8fb08f66e78db9ff
Seeulaterrr <natalyakaplun8@gmail.com>
2020-08-15 16:42:54 +0300

    This is my exercise (String)

---
+++ Lesson_1/Exercise_String.py
@@ -0,0 +1,19 @@
+#Write a program which accepts the user's first
+#and last name and print them in reverse order
+#with a space between them
+
+#name=input("Enter your name ")
+#sur_name=input("Enter your last name ")
+#print(name[::-1] + " " + sur_name[::-1])
+
+#Write a program to accept a file name from the
+#user and print the extension of that
+
+name=input("Enter full file name ")
+print(name[:3])
+
+#
+#
+
+
+
