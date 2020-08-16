a49dcae5fcf8a677a5c6ff58a324ae587ac14e7d
Seeulaterrr <natalyakaplun8@gmail.com>
2020-08-16 11:03:24 +0300

    This program accepts an integer (n) and computes #the value of n+nn+nnn

--- Lesson_1/Exercise_1_5.py
+++ Lesson_1/Exercise_1_5.py
@@ -4,3 +4,14 @@
 n=input("Enter n (integer) ")
 n=int(n)
 print("n=" + str(n) + "\nValue= " + str(n+n*10+n+n*100+n*10+n))
+
+#Write a program to find whether a given number
+# (accept from the user) is even or odd, print out
+# an appropriate message to the user.
+
+m=input("Enter m (integer) ")
+k=int(m)//2
+if (k==1):
+    print("m=" + str(m) + " odd")
+else:
+    print("m=" + str(m) + " even")
