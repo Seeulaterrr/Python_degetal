4d931990c6dca6e30a2f36b4de366842ae6f3e16
Seeulaterrr <natalyakaplun8@gmail.com>
2020-08-16 18:47:47 +0300

    Write a program that will show the menu: #1. Enter number and ** it by 3 #2. Insert 4 IP to list and print it #3. Insert 4 entries to DNS_Dictionary and print it #4. Check if a string is Polindrom

---
+++ Lesson_1/Exrecise_1_13.py
@@ -0,0 +1,17 @@
+# N1: write a program to sum all the items of a list
+# my_list=[2,3,4,5,6,7,8,9]
+# sum=my_list[0]
+# for i in range(1,len(my_list)):
+#     sum=sum+my_list[i]
+
+#print(sum)
+#N2: Write a program to print a specified list after
+#removing the 0, 4 and 5 elements
+
+my_list2=[2,4,6,8,10,12,14,16,18,20]
+print(my_list2)
+my_list2.pop(0)
+my_list2.pop(3)
+my_list2.pop(3)
+print(my_list2)
+
